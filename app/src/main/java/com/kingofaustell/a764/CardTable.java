e05beec0f02f2c3aecc863f4003e2910c64321e8
cbllin7 <chrisbillings89@yahoo.com>
2017-03-28 14:05:36 -0400

    Initial commit

---
+++ app/src/main/java/com/kingofaustell/a764/CardTable.java
@@ -0,0 +1,13 @@
+package com.kingofaustell.a764;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_table);
+    }
+}
